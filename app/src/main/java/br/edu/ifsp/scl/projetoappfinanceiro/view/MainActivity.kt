4e2513f07a350b8bda3ecdf99c1ec64bf09ddf9f
Victor Bueno <vihto8@gmail.com>
2019-10-13 16:41:59 -0300

    Adiciona link para contas na main activity

--- app/src/main/java/br/edu/ifsp/scl/projetoappfinanceiro/view/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/projetoappfinanceiro/view/MainActivity.kt
@@ -1,14 +1,25 @@
 package br.edu.ifsp.scl.projetoappfinanceiro.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.projetoappfinanceiro.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.verContasbtn) {
+            val i = Intent(this, ContaActivity::class.java)
+//            i.putExtra("user", "username")
+            startActivity(i)
+        }
+    }
+
 }
